bd1a09dcbf82dfc8ec972d49d8dfb09888f35135
Yichuan <yichuan.deng@qq.com>
2022-01-03 18:35:42 +0800

    add Database class

--- db.py
+++ db.py
@@ -15,4 +15,32 @@ class Database:
         self.database = settings.POSTGRES_DB
 
         self._pool = None
-        self.con = None
+
+    async def connect(self):
+        if not self._pool:
+            try:
+                self._pool = await create_pool(
+                    min_size=1,
+                    max_size=10,
+                    command_timeout=60,
+                    host=self.host,
+                    port=self.port,
+                    user=self.user,
+                    password=self.password,
+                    database=self.database,
+                )
+            except Exception as e:
+                print(e)
+
+    async def fetch_rows(self, query: str):
+        if not self._pool:
+            await self.connect()
+        else:
+            con = await self._pool.acquire()
+            try:
+                result = await con.fetch(query)
+                return result
+            except Exception as e:
+                print(e)
+            finally:
+                await self._pool.release(con)
